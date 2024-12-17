f537a7c1f2f76e7896d74d6c52d9c16d1c90c4d2
Elias Shamil <eliasshamil@gmail.com>
2024-12-17 11:03:12 +0300

    partial

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-var newItems = ["Buy Food", "Buy Liquid", "Cook Food", "Eat Food"];
+let newItems = ["Buy Food", "Buy Liquid", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get("/", (req, res) => {
   let today = new Date();
@@ -16,21 +17,40 @@ app.get("/", (req, res) => {
     day: "numeric",
     month: "long",
   };
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    dayType: day,
+    listTitle: day,
     item: newItems,
+    copyYear: today.getFullYear(),
   });
 });
 
 app.post("/", (req, res) => {
-  const newList = req.body.addList;
-  newItems.push(newList);
+  let newList = req.body.addList;
+  console.log(req.body);
 
-  res.redirect("/");
+  if (req.body.list == "Work List") {
+    workItems.push(newList);
+    res.redirect("/work");
+  } else {
+    newItems.push(newList);
+    res.redirect("/");
+  }
 });
 
+app.get("/work", (req, res) => {
+  res.render("list", {
+    listTitle: "Work List",
+    item: workItems,
+    copyYear: new Date().getFullYear(),
+  });
+});
+app.post("/work", (req, res) => {
+  let newList = req.body.addList;
+  workItems.push(newList);
+  res.redirect("/work");
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
